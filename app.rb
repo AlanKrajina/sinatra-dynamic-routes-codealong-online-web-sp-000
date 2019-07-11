403b0b345252defcb8bf7b5404d5ec47a4cda879
Alan Krajina <alankrajina88@gmail.com>
2019-07-11 11:45:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -24,7 +24,7 @@ class App < Sinatra::Base
     @user_name1 = params[:num1]
     @user_name2 = params[:num2]
   
-    @product = @user_name1.to_int * @user_name2.to_int
+    @product = @user_name1.to_i * @user_name2.to_i
      "#{product}"
   end
 end
